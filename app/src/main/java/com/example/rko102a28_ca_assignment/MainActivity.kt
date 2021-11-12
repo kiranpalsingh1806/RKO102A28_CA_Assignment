3558fe18e5b6ac9a5c9128269441e682e41d8148
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-11-12 09:05:19 +0530

    Add files

---
+++ app/src/main/java/com/example/rko102a28_ca_assignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rko102a28_ca_assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
