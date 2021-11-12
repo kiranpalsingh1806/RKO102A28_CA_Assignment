026ab970e0c93a57a27cc2c9c6523fa9a3de35f7
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-11-12 09:52:30 +0530

    Add second question files

---
+++ app/src/main/java/com/example/rko102a28_ca_assignment/SecondActivity.kt
@@ -0,0 +1,13 @@
+package com.example.rko102a28_ca_assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity)
+
+    }
+}
